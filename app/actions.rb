434ec21c6cd6d396eb394a30c0593919f6e61a5c
mikmikmac <mpavia121@gmail.com>
2021-06-10 02:10:08 +0000

    w3d1

--- app/actions.rb
+++ app/actions.rb
@@ -8,8 +8,8 @@ end
 
 
 get '/' do 
-    finstagram_post_shark = {
-        username:"sharky_j",
+    @finstagram_post_shark = {
+        username: "sharky_j",
         avatar_url: "http://naserca.com/images/sharky_j.jpg",
         photo_url: "http://naserca.com/images/shark.jpg",
         humanized_time_ago: humanized_time_ago(15),
@@ -21,19 +21,19 @@ get '/' do
         }]
         
     }
-    finstagram_post_whale = {
+    @finstagram_post_whale = {
         username: "kirk_whalum",
         avatar_url: "http://naserca.com/images/kirk_whalum.jpg",
         photo_url: "http://naserca.com/images/whale.jpg",
         humanized_time_ago: humanized_time_ago(65),
         like_count: 0,
-        comment_count: 1,
+        comment_count: 0,
         comments: [{
             username: "kirk_whalum",
             text: "#weekendvibes"
         }]
     }
-    finstagram_post_marlin = {
+    @finstagram_post_marlin = {
         username: "marlin_peppa",
         avatar_url: "http://naserca.com/images/marlin_peppa.jpg",
         photo_url: "http://naserca.com/images/marlin.jpg",
@@ -45,6 +45,6 @@ get '/' do
             text: "lunchtime! ;)"
         }]
     }
-
-    [finstagram_post_shark, finstagram_post_whale, finstagram_post_marlin].to_s
+    @finstagram_posts = [@finstagram_post_shark, @finstagram_post_whale, @finstagram_post_marlin]
+    erb(:index)
 end
